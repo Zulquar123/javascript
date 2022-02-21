9350db09849ad68cdb21bd1f9e345817a02f7c81
Zulquar123 <zulquar150@gmail.com>
2022-02-21 22:28:46 +0300

    add

--- function.js
+++
@@ -1,8 +0,0 @@
-function addition(a,b){
-    const result = a+b;
-    return result;
-}
-
-const x = 10;
-const y= 15;
-addition(x,y);
---
+++ function_add.js
@@ -0,0 +1,9 @@
+function addition(a, b) {
+  const result = a + b;
+  return result;
+}
+
+const x = 10;
+const y = 15;
+const add = addition(x, y);
+console.log(add);
